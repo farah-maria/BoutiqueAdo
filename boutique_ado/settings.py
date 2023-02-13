717265e4a2f0d1b78ddb5e93d9f5fd5512453c87
Farah M <farah.maria.rahman@gmail.com>
2023-02-13 22:01:17 +0000

    create checkout app and models

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_countries',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
 ]
  
 MIDDLEWARE = [
